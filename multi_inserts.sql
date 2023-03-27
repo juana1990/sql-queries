7b5e832866e37595b0ff9d40106fa4338d7432c7
Joanna Frankowska <46848302+juana1990@users.noreply.github.com>
2023-03-27 14:54:28 +0200

    multi_inserts

---
+++ multi_inserts.sql
@@ -0,0 +1,14 @@
+-- Single insert (switching order of name and age)
+
+INSERT INTO cats (age, name) 
+VALUES 
+  (2, 'Candy');
+
+
+-- Multiple Insert:
+
+INSERT INTO cats (name, age) 
+VALUES 
+  ('Luna', 5), 
+  ('Baily', 1), 
+  ('Heaven', 15);
